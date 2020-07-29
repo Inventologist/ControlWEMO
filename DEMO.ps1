e65c3af91ea5d310a570dc65542be4891be42eab
Ben Therien <cs135cid@yahoo.com>
2020-07-28 22:00:47 -0400

    Create DEMO.ps1

---
+++ DEMO.ps1
@@ -0,0 +1,6 @@
+﻿#Retrieve required Dependencies
+Write-Host "Gathering Dependencies" -f Green
+Invoke-Expression ('$GHDLUri="https://github.com/Inventologist/ControlWEMO/archive/master.zip";$GHUser="Inventologist";$GHRepo="ControlWEMO";$ForceRefresh = "Yes"' + (new-object net.webclient).DownloadString('https://raw.githubusercontent.com/Inventologist/Get-Git/master/Get-Git.ps1'))
+
+Set-WemoOn 192.168.1.81 | Out-Null
+Set-WemoOff 192.168.1.81 | Out-Null
